e482bbb8fae61bf94219ac450ebeea4115250173
Sujith Manikandan <jith013@gmail.com>
2025-05-18 18:04:42 -0400

    added filtering, sorting and pagination through query parameters

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0e12e95c1889b18b119c47b1e779b9272beb65a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c98913aab42140b04462d7b3ebf8330311fd017")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
